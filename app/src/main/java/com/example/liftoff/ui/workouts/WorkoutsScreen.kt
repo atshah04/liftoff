93213e3af711ff9e962187dbd7bc3692bce86d3c
Utkarsh Modgil <umodgil@uwaterloo.ca>
2024-10-19 19:59:50 -0400

    display workouts from db

--- app/src/main/java/com/example/liftoff/ui/workouts/WorkoutsScreen.kt
+++ app/src/main/java/com/example/liftoff/ui/workouts/WorkoutsScreen.kt
@@ -8,12 +8,14 @@ import androidx.compose.ui.unit.*
 import androidx.compose.foundation.border
 import androidx.compose.ui.graphics.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.*
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-
-data class Workout(val name: String, val reps: Int, val sets: Int)
-data class WorkoutSession(val exercises: List<Workout>)
+import com.example.liftoff.data.dto.WorkoutDto
+import com.example.liftoff.data.repository.WorkoutRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 val default_mod = Modifier
     .clip(RoundedCornerShape(2.dp))
@@ -22,34 +24,16 @@ val default_mod = Modifier
     .fillMaxWidth(0.9f)
 
 @Composable
-fun WorkoutsScreen() {
+fun WorkoutsScreen(db : WorkoutRepository) {
+    var workouts by remember { mutableStateOf(listOf<WorkoutDto>()) }
 
-    var data: List<WorkoutSession> = listOf(
-        WorkoutSession(exercises = listOf(
-            Workout("Push Ups", 15, 3),
-            Workout("Pull Ups", 10, 4),
-            Workout("Squats", 20, 4),
-            Workout("Bench Press", 12, 3),
-            Workout("Deadlift", 8, 3))),
-        WorkoutSession(exercises = listOf(
-            Workout(name = "Bicep Curls", reps = 12, sets = 3),
-            Workout(name = "Tricep Dips", reps = 15, sets = 3),
-            Workout(name = "Lunges", reps = 20, sets = 4),
-            Workout(name = "Plank", reps = 1, sets = 3),
-            Workout(name = "Burpees", reps = 15, sets = 3))),
-        WorkoutSession(exercises = listOf(
-            Workout("Push Ups", 12, 2),
-            Workout("Pull Ups", 8, 3),
-            Workout("Squats", 6, 2),
-            Workout("Bench Press", 4, 1),
-            Workout("Deadlift", 4, 2))),
-        WorkoutSession(exercises = listOf(
-            Workout(name = "Bicep Curls", reps = 12, sets = 2),
-            Workout(name = "Tricep Dips", reps = 8, sets = 2),
-            Workout(name = "Lunges", reps = 20, sets = 4),
-            Workout(name = "Plank", reps = 1, sets = 3),
-            Workout(name = "Burpees", reps = 15, sets = 3))),
-    )
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.IO) {
+            // for now, we hard code fetching workouts done by userId 6
+            // db.createWorkout(WorkoutDto(6,"timed","test before commit",15))
+            workouts = db.getWorkoutsByUserId(userId = 6)
+        }
+    }
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -67,33 +51,46 @@ fun WorkoutsScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth().padding(top = 80.dp)
         ) {
-            DynamicColumn(items = data)
+            DynamicColumn(items = workouts)
         }
     }
 }
 
 @Composable
-fun Card(session: WorkoutSession) {
-
+fun Card(session: WorkoutDto) {
     Column(modifier = default_mod) {
         Row(modifier = Modifier.fillMaxWidth()) {
             Text(text = "Exercise", modifier = Modifier.weight(1f))
-            Text(text = "Reps", modifier = Modifier.weight(0.5f))
-            Text(text = "Sets", modifier = Modifier.weight(0.5f))
-        }
-        session.exercises.forEach { workout ->
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Text(text = workout.name, modifier = Modifier.weight(1f))
-                Text(text = workout.reps.toString(), modifier = Modifier.weight(0.5f))
-                Text(text = workout.sets.toString(), modifier = Modifier.weight(0.5f))
+
+            if (session.workoutType == "strength") {
+                Text(text = "Reps", modifier = Modifier.weight(0.5f))
+                Text(text = "Sets", modifier = Modifier.weight(0.5f))
+            } else if (session.workoutType == "timed") {
+                Text(text = "Duration", modifier = Modifier.weight(1f))
             }
         }
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(text = session.workoutName, modifier = Modifier.weight(1f))
 
+            when (session.workoutType) {
+                "strength" -> {
+                    Text(text = session.reps?.toString() ?: "N/A", modifier = Modifier.weight(0.5f))
+                    Text(text = session.sets?.toString() ?: "N/A", modifier = Modifier.weight(0.5f))
+                }
+                "timed" -> {
+                    Text(text = session.duration?.toString() ?: "N/A", modifier = Modifier.weight(1f))
+                }
+                else -> {
+                    Text(text = "Unknown Type", modifier = Modifier.weight(1f))
+                }
+            }
+        }
     }
 }
 
+
 @Composable
-fun DynamicColumn(items: List<WorkoutSession>) {
+fun DynamicColumn(items: List<WorkoutDto>) {
     Column {
         items.forEach { session ->
                 Card(session)
